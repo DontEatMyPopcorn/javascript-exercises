7e55d9832d51fec0637d90b784c93216850bd5a8
DontEatMyPopcorn <173909633+DontEatMyPopcorn@users.noreply.github.com>
2024-07-06 18:16:10 -0400

    Completed JS exercise 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(term,quantity) {
+    if (quantity < 0) {
+        return "ERROR";
+    }
+    else {
+        let output;
+        output = "";
+        for (let i = 0; i < quantity; i++) {
+            output = output + term;
+        }
+        return output;
+    }
 };
 
 // Do not edit below this line
