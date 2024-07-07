9ce514376222f88957604550a545feb065039952
DontEatMyPopcorn <173909633+DontEatMyPopcorn@users.noreply.github.com>
2024-07-07 13:14:33 -0400

    Completed JS exercise 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let inputArray = [];
+    let inputLength = inputString.length;
+    inputLength = inputLength - 1;
+    for (let i = inputLength;i > -1; i--) {
+        inputArray.push(inputString.charAt(i));
+    }
+    let reverse = "";
+    for (let j = 0; j < inputString.length; j++) {
+        reverse = reverse + inputArray[j];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
