516c0cf19e75f0c0f98eb8336193f40da8b9ad36
DontEatMyPopcorn <173909633+DontEatMyPopcorn@users.noreply.github.com>
2024-07-07 16:29:03 -0400

    Complted JS exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+const sumAll = function(in1, in2) {
+    if (typeof(in1)!== "number") {
+        return "ERROR"
+    }
+    if (typeof(in2)!== "number") {
+        return "ERROR"
+    }
+    if (in1 < 0) {
+        return "ERROR"
+    }
+    if (in2 < 0) {
+        return "ERROR"
+    }
+    
+    let output = 0;
+    let bigger;
+    let smaller;
+    if (in1 > in2) {
+        bigger = in1;
+        smaller = in2;
+    }
+    else if (in1 < in2) {
+        smaller = in1;
+        bigger = in2;
+    }
+    //else if in1 == in2
+
+    for (let i = smaller; i <= bigger; i++){
+        output += i;
+    }
+
+    return output;
 
 };
 
