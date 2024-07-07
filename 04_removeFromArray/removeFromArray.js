984a9a32cae6e4f1a1fdd39c62d0e9f3a4ef5496
DontEatMyPopcorn <173909633+DontEatMyPopcorn@users.noreply.github.com>
2024-07-07 14:13:48 -0400

    Completed JS exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, in1, in2, in3, in4) {
+    let arrayLength;
+    arrayLength = inputArray.length;
+    for (let i = arrayLength; i > -1; i--) {
+        if (inputArray[i] === in1) {
+            inputArray.splice(i,1);
+        }
+    }
+
+    arrayLength = inputArray.length;
+    for (let i = arrayLength; i > -1; i--) {
+        if (inputArray[i] === in2) {
+            inputArray.splice(i,1);
+        }
+    }
+
+    arrayLength = inputArray.length;
+    for (let i = arrayLength; i > -1; i--) {
+        if (inputArray[i] === in3) {
+            inputArray.splice(i,1);
+        }
+    }
+
+    arrayLength = inputArray.length;
+    for (let i = arrayLength; i > -1; i--) {
+        if (inputArray[i] === in4) {
+            inputArray.splice(i,1);
+        }
+    }
+    
+    return inputArray;
 };
 
 // Do not edit below this line
