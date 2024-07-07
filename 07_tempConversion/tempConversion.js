c982a0b8029a04a2c41732459e684affe77edeef
DontEatMyPopcorn <173909633+DontEatMyPopcorn@users.noreply.github.com>
2024-07-07 16:52:03 -0400

    Completed JS exercise 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let deg;
+  deg = (temp -32) * (5/9);
+  deg = Math.round (deg*10) / 10;
+  return deg;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let deg;
+  deg = temp * 9 / 5 + 32;
+  deg = Math.round (deg*10) / 10;
+  return deg;
 };
 
 // Do not edit below this line
