9af974eb32200b8ef9561571245d3d9fcd815d54
DontEatMyPopcorn <173909633+DontEatMyPopcorn@users.noreply.github.com>
2024-07-07 16:44:57 -0400

    Completed JS exercise 06.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let output = true;
+    let mod4 = year%4;
+    let mod100 = year%100;
+    let mod400 = year%400;
+
+    if (mod4 !== 0) {
+        output = false;
+    }
+    if (mod100 === 0 & mod400 !== 0) {
+        output = false;
+    }
+    return output;
 
 };
 
