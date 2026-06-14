9cb2b8a8427daee4a146f339d8ae01a4e76df4a7
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-14 12:29:08 -0400

    Complete 11_tempConversion.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  let output = 0.0;
+  output = input - 32;
+  output = output / 9 * 5;
+  output = (Math.round(output*10)/10);
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  let output = 0.0;
+  output = input * 9 / 5;
+  output = output + 32;
+  output = (Math.round(output*10)/10);
+  return output;
 };
 
 // Do not edit below this line
