dc77f8cc1accbab576245b991bb16b49f68d61ff
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-14 11:16:43 -0400

    Complete 08_removeFromArray.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...removals) {
+
+    let output = input;
+    for (let i = 1; i < arguments.length; i++){
+        output = output.filter(num => num !== arguments[i]);
+
+    }
+
+    return output;
 };
 
 // Do not edit below this line
