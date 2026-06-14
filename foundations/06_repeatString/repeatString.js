124f5359095d54abafa96e27c0e4f6446852deb9
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-14 10:23:32 -0400

    Complete 06_repeatString.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(input, count) {
+    let arr = [];
+    
+    for (let i = 0; i < count; i++) {
+        arr.push(input);
+    }
 
+    let output = "";
+    if (count >= 1) {
+        output = arr.reduce((accumulator, currentItem) => accumulator += input);
+    } else if (count === 0) {
+        output = "";
+    } else {
+        output = "ERROR";
+    }
+    return output;
 };
 
 // Do not edit below this line
