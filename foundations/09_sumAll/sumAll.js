ee94a57023f1343d0deba854634370c669f3c87e
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-14 12:13:04 -0400

    Complete 09_sumAll.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(first,second) {
+    let output = 0;
+    let lower = 0;
+    let higher = 0;
 
+    if (first < 0 || second < 0){
+        output = "ERROR";
+    } else if (typeof first !== "number" || typeof second !== "number") {
+        output = "ERROR";
+    } else if (first % 1 !== 0 ||  second % 1 !== 0) {
+        output = "ERROR";
+    } else {
+        if (first <= second){
+            lower = first;
+            higher = second;
+        } else {
+            lower = second;
+            higher = first;
+        }
+
+        for (let i = lower; i <= higher; i++){
+            output += i;
+        }
+    }
+
+    return output;
 };
 
 // Do not edit below this line
