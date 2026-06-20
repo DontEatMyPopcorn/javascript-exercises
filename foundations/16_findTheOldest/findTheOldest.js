4f83aca0cf048f3317fed7911a9f57c0b6f350e8
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-20 18:52:38 -0400

    Completed exercises.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+console.log(people);
+const defaults = { yearOfDeath: 2026};
+people.map(person=>
+// Merge defaults with the incomplete object
+Object.assign(person, { ...defaults, ...person })
+);
+console.log(people);
 
+    let sorted = people.sort((a,b)=> b.yearOfDeath-b.yearOfBirth-a.yearOfDeath+a.yearOfBirth)
+    let oldest = sorted[0];
+    return oldest;
 };
-
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
