4fa54ecc3119bc83482ede4d1c17425e0cf7bd71
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-20 18:16:19 -0400

    Complete exercise.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(user) {
+    let input = Number(user);
+    let output = 1;
+    let previous = 0;
+    if (input >= 1){
+        for (let i = 2; i <= input; i++){
+            output = output + previous;
+            previous = output - previous;
+        }
+    } else if (input === 0){
+        output = 0;
+    } else if (input < 0){
+        output = "OOPS";
+    }
+    return output;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
