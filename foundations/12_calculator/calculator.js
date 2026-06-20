19dc22f8442e233cc7f1104a181afa871c302904
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-20 18:01:18 -0400

    Complete a couple exercises.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+	return input.reduce((accumulator, input) => accumulator + input,
+  0,
+);
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+	return input.reduce((accumulator, input) => accumulator * input,
+  1,
+);
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	let output = input;
+  let j = input - 1;
+
+  if (input>0){
+    for (let i = j; i >1; i--){
+      output = output * i;
+    }
+  } else {
+    output = 1;
+  }
+  return output;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let temp = phrase.replace(/[^a-zA-Z0-9]/g, ''); 
+    let input = temp.toLowerCase();
+    let answer = true;
 
+    for (let i = 0; i <= input.length-1; i++){
+        if (input[i] !== input[input.length-i-1]){
+            answer = false;
+        }
+        console.log("i", i);
+        console.log(input[i], input[input.length-i-1]);
+        console.log(answer);
+    }
+
+
+    return answer;
 };
-
+palindromes('A car, a man, a maraca.')
 // Do not edit below this line
 module.exports = palindromes;
